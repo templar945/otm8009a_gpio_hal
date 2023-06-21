51088cfde848bd3fcaeff50c020d2a1c330f9bf9
templar945 <42736293+templar945@users.noreply.github.com>
2023-06-22 04:36:25 +0800

    Create otm8009a_gpio.h

---
+++ otm8009a_gpio.h
@@ -0,0 +1,7 @@
+#define white       0xFFFF
+#define black       0x0000
+#define blue      	0x001F
+#define red         0xF800
+#define yellow      0xFFE0
+#define brown       0XBC40
+#define green       0x07E0
